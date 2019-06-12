1804abdf01c967de816f4e7a67f918bd18da15d7
Jcraw <jeff.j.crawford07@gmail.com>
2019-06-12 15:59:47 -0700

    Fix ???
    61219

---
+++ Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingUVs_Layers : MonoBehaviour 
+{
+	//public int materialIndex = 0;
+	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
+	public string textureName = "_MainTex";
+	
+	Vector2 uvOffset = Vector2.zero;
+	
+	void LateUpdate() 
+	{
+        float x = Time.deltaTime;
+
+        uvOffset += ( uvAnimationRate * (x/4));
+		if( GetComponent<Renderer>().enabled )
+		{
+			GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, uvOffset );
+		}
+	}
+}
